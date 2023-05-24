c28654b88a2a9e62add9b1b898ace5edde556ab0
RKanna <rkannan.balakrishnan@hotmail.com>
2023-05-24 10:17:28 +0530

    Added let instead of var

--- sum.js
+++ sum.js
@@ -1,6 +1,6 @@
 function functionCalc() {
-  var elementX = parseInt(document.getElementById(`VariableX`).value);
-  var elementY = parseInt(document.getElementById(`VariableY`).value);
-  var sum = elementX + elementY;
+  let elementX = parseInt(document.getElementById(`VariableX`).value);
+  let elementY = parseInt(document.getElementById(`VariableY`).value);
+  let sum = elementX + elementY;
   document.getElementById(`result`).textContent = "Result is" + " " + sum;
 }
